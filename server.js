99f78738520b500f1867eb795d49885819f47066
cynthia <santoro.cynthia@yahoo.com>
2022-10-24 22:10:01 -0700

    push

---
+++ server.js
@@ -0,0 +1,57 @@
+//dependencies
+const express = require('express');
+const app = express();
+const methodOverride = require("method-override");
+const PORT = process.env.PORT
+//data
+const budget = require('./models/budget.js');
+
+
+// body-parser
+app.use(express.static('public')); //statically match with file named public
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
+
+const sum = () => {
+  let bank = 0
+  for (let i = 0; i < budget.length; i++) {
+    bank += parseFloat(budget[i].amount);
+  }
+  bank = parseFloat(bank); 
+
+  return bank;
+}
+
+// INDEX route
+app.get('/budgets', (req, res) => {
+  res.render('index.ejs', { 
+    budget: budget,
+    bank: sum()
+  });
+
+});
+
+// new route Note: new route must be above show route
+app.get('/budgets/new', (req, res) => {
+  res.render('new.ejs');
+});
+
+app.post('/budgets' , (req,res) => {
+  req.body.amount = parseFloat(req.body.amount); 
+  budget.push(req.body)
+  res.redirect('/')
+})
+
+
+// SHOW route
+app.get('/budgets/:index', (req, res) => {
+  res.render('show.ejs', {
+      budget: budget[req.params.index],
+  });
+});
+
+// Listener
+app.listen(PORT, ()=> console.log(`You are listening to the smoothe sounds of port ${PORT}...`))
+  
+  
